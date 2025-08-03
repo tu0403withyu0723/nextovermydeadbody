609507884139064ba6a48bc54c94b3fc306ba281
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-04 02:27:09 +0800

    20250804 v1

---
+++ git-nextovermydeadbody.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "20250804 v1"
+git checkout -b main
+git remote add origin https://github.com/tu0403withyu0723/nextovermydeadbody.git
+git push -u origin main --force
+pause
